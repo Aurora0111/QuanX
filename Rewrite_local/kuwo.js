22b7ee3332a6f0db3bc77d8a7c27838c0af2b9f7
junmoxio <56946309+junmoxio@users.noreply.github.com>
2022-01-18 14:15:24 +0800

    Update kuwo.js

--- Rewrite_local/kuwo.js
+++ Rewrite_local/kuwo.js
@@ -8,10 +8,10 @@ const vip = '/vip/v2/user/vip';
 const time = '/vip/spi/mservice';
 
 if (url.indexOf(vip) != -1) {
-	obj.data["isNewUser"] = "6";
+	obj.data["isNewUser"] = "2";
 	obj.data["vipLuxuryExpire"] = "4092647115000";
 	obj.data["time"] = "4092647115000";
-	obj.data["isYearUser"] = "6";
+	obj.data["isYearUser"] = "2";
 	obj.data["vipmExpire"] = "4092647115000";
 	obj.data["vipOverSeasExpire"] = "4092647115000";
 	obj.data["vipExpire"] = "4092647115000";
@@ -20,8 +20,8 @@ if (url.indexOf(vip) != -1) {
 }
 
 if (url.indexOf(time) != -1) {
-	obj["isVIPMAutoPay"] = 6;
-	obj["isVIPLuxAutoPay"] = 6;
+	obj["isVIPMAutoPay"] = 2;
+	obj["isVIPLuxAutoPay"] = 2;
 	body = JSON.stringify(obj);
 }
 
